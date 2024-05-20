7d1867c683889e144199bb59300f7481a040fe6f
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2024-05-20 19:35:18 +0200

    API endpoints for projects

---
+++ server/api/projects/[id].ts
@@ -0,0 +1,35 @@
+import {serverSupabaseClient} from "#supabase/server";
+import {Database} from "~/types/supabase";
+
+/**
+ * This endpoint returns a single project by id.
+ */
+export default defineEventHandler(async (event) => {
+  // Extract the id from the event context
+  const id = event.context.params?.id;
+
+  // If the id is missing, return a 400 error. This should not happen since there is an index.ts file that handles the root path.
+  if (!id) {
+    throw createError({statusCode: 400, message: 'Missing id'});
+  }
+
+  const client = await serverSupabaseClient<Database>(event);
+
+  // Query the project by id
+  const {data, error} = await client.from('projects').select(
+    'id, name, abstract, description,' +
+    'main_image: images!main_image_id(label, path),' +
+    'other_images: images!project_other_image(label, path),' +
+    'main_responsible: people!projects_main_responsible_id_fkey(id, name, surname, email, picture: images(label, path)),' +
+    'people_involved: people!project_person_involved(id, name, surname, email, picture: images(label, path))'
+  )
+    .eq('id', id)
+    .single();
+
+  // If there is an error in the query, return a 400 error
+  if (error) {
+    throw createError({statusCode: 400, message: error.message});
+  }
+
+  return data;
+})
---
+++ server/api/projects/index.ts
@@ -0,0 +1,22 @@
+import {serverSupabaseClient} from "#supabase/server";
+import {Database} from "~/types/supabase";
+
+/**
+ * This endpoint returns all projects.
+ */
+export default defineEventHandler(async (event) => {
+  const client = await serverSupabaseClient<Database>(event);
+
+  // Get all projects with minimal information
+  const {data, error} = await client.from('projects').select(
+    'id, name, abstract,' +
+    'main_image: images!main_image_id(label, path)'
+  );
+
+  // If there is an error in the query, return a 400 error
+  if (error) {
+    throw createError({statusCode: 400, message: error.message});
+  }
+
+  return data;
+})
