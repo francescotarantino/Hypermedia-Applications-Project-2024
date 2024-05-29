3c8cee23a5cd094be5873515571bc84a4bedb97d
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2024-05-29 15:13:53 +0200

    Typing indicator in AppChatbot

--- components/AppChatbot.vue
+++ components/AppChatbot.vue
@@ -21,17 +21,24 @@
             {{ message.text }}
           </div>
         </div>
+        <!-- Typing Indicator -->
+        <div v-if="typing" class="text-left bg-peach p-2 rounded-md opacity-50">
+          <p class="text-sm italic">Typing...</p>
+        </div>
       </div>
       <!-- Input Field -->
       <div class="h-px bg-primary"/>
       <div class="p-2 bg-peach rounded-b-lg">
-        <input v-model="userInput" @keydown.enter="submitMessage" type="text" placeholder="Type a message..." class="w-full bg-cream p-2 rounded-md"/>
+        <input v-model="userInput" @keydown.enter="submitMessage" type="text" placeholder="Type a message..." class="w-full bg-cream p-2 rounded-md" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+
+const RESPONSE_CHECK_INTERVAL = 1000;
+
 export default {
   data() {
     return {
@@ -42,6 +49,7 @@ export default {
       runId: null, // To store the run ID
       runStatus: null, // To store the run status
       taskEnded: true, // Indicates if the current task has ended
+      typing: false, // Indicates if the AI is typing
       interval: null, // To store the interval for checking the run status
     };
   },
@@ -53,7 +61,7 @@ export default {
       // If the conversation thread has not been initiated, initiate it
       if (this.threadId === null) {
         // Push a welcome message when the chat is opened
-        this.messages.push({ text: 'Hello! Welcome to Signal for Help Empowerment (SHE). How can I support you today?', isUser: false });
+        this.pushMessage('Hello! Welcome to Signal for Help Empowerment (SHE). How can I support you today?');
 
         this.initiateConversationThread();
       }
@@ -80,29 +88,27 @@ export default {
         this.threadId = data.threadId;
       } catch (error) {
         // Handle error by displaying a message to the user
-        this.messages.push({ text: 'An error occurred while communicating with the AI. Please try again later.', isUser: false });
-        this.taskEnded = true;
+        this.pushMessage('An error occurred while communicating with the AI. Please try again later.');
       }
     },
 
     // Handles user input when the user presses the Enter key
-    async submitMessage() {
+    submitMessage() {
       if (this.userInput.trim() !== '' && this.taskEnded) {
         this.taskEnded = false;
-        // Add user message to the messages array
-        this.messages.push({ text: this.userInput, isUser: true });
-        // Scroll to the bottom of the chat
-        this.scrollToBottom();
+        this.pushMessage(this.userInput, true);
+
         // Store user input locally before clearing it
         const userInput = this.userInput;
         this.userInput = ''; // Clear the input field
-        await this.sendMessage(userInput);
+
+        this.sendMessage(userInput);
       }
     },
 
     // Sends the user message to the AI and handles the response
     async sendMessage(userInput) {
-      this.runStatus = "queuing";
+      this.runStatus = "queued";
 
       try {
         // Call Assistant API to send the message
@@ -118,7 +124,8 @@ export default {
         this.retrieveRun();
       } catch (error) {
         // Handle error by displaying a message to the user
-        this.messages.push({ text: 'An error occurred while communicating with the AI. Please try again later.', isUser: false });
+        this.pushMessage('An error occurred while communicating with the AI. Please try again later.');
+
         this.taskEnded = true;
       }
     },
@@ -126,12 +133,17 @@ export default {
     // Retrieves the run status and waits for completion
     retrieveRun() {
       this.interval = setInterval(async () => {
+        // Display typing indicator
+        this.typing = true;
+        this.scrollToBottom();
+
         const response = await fetch(`/api/chatbot/${this.threadId}/readMessage/${this.runId}`, {
           method: 'GET',
         });
 
         const data = await response.json();
 
+        // If the previous run status is completed or failed, stop the interval, to prevent the same message from being pushed multiple times
         if (this.runStatus === 'completed' || this.runStatus === 'failed') {
           clearInterval(this.interval);
           return;
@@ -145,16 +157,19 @@ export default {
         }
 
         if (this.runStatus === 'completed') {
-          this.messages.push({ text: data.message, isUser: false });
-          this.scrollToBottom();
-          setTimeout(() => {
-            this.taskEnded = true;
-          }, 1000);
+          this.typing = false;
+          this.pushMessage(data.message);
         } else if (this.runStatus === 'failed') {
           this.sendMessage(this.userInput);
-          this.taskEnded = true;
         }
-      }, 1000);
+      }, RESPONSE_CHECK_INTERVAL);
+    },
+
+    pushMessage(text, isUser = false) {
+      // Add user message to the messages array
+      this.messages.push({ text, isUser });
+      // Scroll to the bottom of the chat
+      this.scrollToBottom();
     },
 
   },
