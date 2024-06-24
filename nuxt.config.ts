2884a108bd7aaa72369cf6b4e3a5311e38675068
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2024-06-24 20:31:13 +0200

    Added title and logo head tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Signal for Help Empowerment centre",
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/logo.png' },
+      ]
+    },
+  },
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase", "@nuxt/fonts"],
   supabase: {
