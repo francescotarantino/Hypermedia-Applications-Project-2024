48dfe61031ed30465b2d74791065493294b01d4b
carloprestifilippo <carlo.prestifilippo@mail.polimi.it>
2024-06-26 12:36:06 +0200

    changed dropdown padding

--- components/AppDropdownContent.vue
+++ components/AppDropdownContent.vue
@@ -8,9 +8,9 @@ const dropdownActive = inject('dropdownActive');
     <!-- Wrapping div with conditional rendering based on 'active' state -->
     <div v-if="dropdownActive">
       <!-- Dropdown menu container -->
-      <div class="origin-top-right bg-cream text-primary absolute right-0 mt-2 w-full rounded-md ring-1 ring-primary ring-opacity-10">
+      <div class="origin-top-right bg-cream text-primary absolute right-0 mt-2 w-full rounded-md drop-shadow-xl">
         <!-- Dropdown menu items -->
-        <div class="p-2" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+        <div class="m-2" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
           <!-- Inserting slot content here -->
           <slot />
         </div>
--- components/AppDropdownLink.vue
+++ components/AppDropdownLink.vue
@@ -7,7 +7,7 @@ const toggleMenu = inject<() => void>('toggleMenu');
 </script>
 
 <template>
-  <NuxtLink :to="to" @click="toggleMenu" class="block px-4 py-2 bg-cream rounded-lg hover:bg-peach transition ease-in-out duration-200" role="menuitem">
+  <NuxtLink :to="to" @click="toggleMenu" class="block p-2 bg-cream rounded-lg hover:bg-peach transition ease-in-out duration-200" role="menuitem">
     <p class="text-primary text-lg">
       <slot />
     </p>
