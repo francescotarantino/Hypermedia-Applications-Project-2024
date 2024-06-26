b2c44d88f32aa172b0ce3a0b015803cf1d2db56e
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2024-06-26 11:17:25 +0200

    Chatbot refinements with transitions and disclaimer

--- components/AppChatbot.vue
+++ components/AppChatbot.vue
@@ -1,178 +1,200 @@
 <template>
   <div>
-    <!-- Chatbot Icon (visible when the chat is closed) -->
-    <div v-if="!isOpen" @click="toggleChat" class="fixed bottom-4 right-4 cursor-pointer" style="z-index: 1000000">
-      <img src="/chatbot.png" alt="Chatbot Icon" class="w-16 md:w-24 h:16 md:h-24 rounded-full shadow-lg"/>
-    </div>
-    <!-- Chatbot Window -->
-    <div v-if="isOpen" class="fixed bottom-4 right-4 left-4 md:left-auto bg-white w-auto md:w-1/2 xl:w-1/4 h-4/5 md:h-3/4 lg:h-2/3 shadow-lg rounded-lg flex flex-col border border-primary" ref="chatContainer" style="z-index: 999999">
-      <!-- Header -->
-      <div class="p-1 bg-apricot flex justify-between items-center rounded-t-lg">
-        <img src="/chatbot.png" alt="Chatbot Icon" class="w-12 h-12 rounded-full shadow-lg border border-primary"/>
-        <h3 class="text-lg">SHE-helper</h3>
-        <button @click="toggleChat" class="font-bold pr-6">&times;</button>
+    <transition name="fade">
+      <!-- Chatbot Icon (visible when the chat is closed) -->
+      <div v-if="!isOpen" @click="toggleChat" class="fixed bottom-4 right-4 cursor-pointer hover:scale-105 transition ease-in-out duration-200" style="z-index: 1000000">
+        <img src="/chatbot.png" alt="Chatbot Icon" class="w-16 md:w-24 h:16 md:h-24 rounded-full shadow-lg"/>
       </div>
-      <!-- Divider -->
-      <div class="h-px bg-primary z-40"></div>
-      <!-- Chat Messages Container -->
-      <div class="flex-grow p-4 overflow-y-auto" ref="messagesContainer">
-        <div v-for="(message, index) in messages" :key="index" class="mb-4">
-          <div :class="message.isUser ? 'text-right bg-cream' : 'text-left bg-peach'" class="p-2 rounded-md">
-            {{ message.text }}
+    </transition>
+
+    <transition name="fade">
+      <!-- Chatbot Window -->
+      <div v-if="isOpen" class="fixed bottom-4 right-4 left-4 md:left-auto bg-white w-auto md:w-1/2 xl:w-1/4 h-4/5 md:h-3/4 lg:h-2/3 shadow-lg rounded-lg flex flex-col border border-primary" ref="chatContainer" style="z-index: 999999">
+        <!-- Header -->
+        <div class="p-1 bg-apricot flex justify-between items-center rounded-t-lg">
+          <img src="/chatbot.png" alt="Chatbot Icon" class="w-12 h-12 rounded-full shadow-lg border border-primary"/>
+          <h3 class="text-lg">SHE-helper</h3>
+          <button @click="toggleChat" class="pr-3 hover:opacity-70"><XMarkIcon class="size-6 text-primary" /></button>
+        </div>
+
+        <!-- Divider -->
+        <div class="h-px bg-primary z-40" />
+
+        <!-- Chat Messages Container -->
+        <div class="flex-grow p-4 overflow-y-auto" ref="messagesContainer">
+          <div v-for="(message, index) in messages" :key="index" class="mb-4">
+            <div :class="message.isUser ? 'text-right bg-cream' : 'text-left bg-peach'" class="p-2 rounded-md">
+              {{ message.text }}
+            </div>
+          </div>
+          <!-- Typing Indicator -->
+          <div v-if="typing" class="text-left bg-peach p-2 rounded-md opacity-50">
+            <p class="text-sm italic">Typing...</p>
           </div>
         </div>
-        <!-- Typing Indicator -->
-        <div v-if="typing" class="text-left bg-peach p-2 rounded-md opacity-50">
-          <p class="text-sm italic">Typing...</p>
+
+        <!-- Disclaimer -->
+        <p class="text-xs opacity-50 text-center">Disclaimer: You are chatting with a virtual assistant, powered by AI. The information provided is not a substitute for professional advice.</p>
+
+        <!-- Input Field -->
+        <div class="h-px bg-primary" />
+        <div class="p-2 bg-peach rounded-b-lg">
+          <input v-model="userInput" @keydown.enter="submitMessage" type="text" placeholder="Type a message..." class="w-full bg-cream p-2 rounded-md" />
         </div>
       </div>
-      <!-- Input Field -->
-      <div class="h-px bg-primary"/>
-      <div class="p-2 bg-peach rounded-b-lg">
-        <input v-model="userInput" @keydown.enter="submitMessage" type="text" placeholder="Type a message..." class="w-full bg-cream p-2 rounded-md" />
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
+import { XMarkIcon } from "@heroicons/vue/24/outline";
 
 const RESPONSE_CHECK_INTERVAL = 1000;
+const FIRST_MESSAGE = 'Hello! Welcome to Signal for Help Empowerment (SHE). How can I support you today?';
+const ERROR_MESSAGE = 'An error occurred while communicating with the AI. Please try again later.';
 
-export default {
-  data() {
-    return {
-      isOpen: false, // Indicates if the chat window is open
-      userInput: '', // Binds to the user's input in the text field
-      messages: [], // Array to store chat messages
-      threadId: null, // To store the conversation thread ID
-      runId: null, // To store the run ID
-      runStatus: null, // To store the run status
-      taskEnded: true, // Indicates if the current task has ended
-      typing: false, // Indicates if the AI is typing
-      interval: null, // To store the interval for checking the run status
-    };
-  },
+const isOpen = ref(false); // Indicates if the chat window is open
+const userInput = ref(''); // Binds to the user's input in the text field
+const messages = ref<{
+  text: string;
+  isUser: boolean;
+}[]>([]); // Array to store chat messages
+const threadId = ref(null); // Stores the conversation thread ID
+const runId = ref(null); // Stores the run ID
+const runStatus = ref<
+    "queued" | "in_progress" | "completed" | "failed" | null
+>(null); // Stores the run status
+const taskEnded = ref(true); // Indicates if the current task has ended
+const typing = ref(false); // Indicates if the AI is typing
+const interval = ref<NodeJS.Timeout | null>(null); // Stores the interval for checking the run status
+const messagesContainer = ref<HTMLDivElement>(); // Reference to the chat messages container
 
-  methods: {
+// Toggles the chat window visibility
+function toggleChat() {
+  // If the conversation thread has not been initiated, initiate it
+  if (threadId.value === null) {
+    // Push a welcome message when the chat is opened
+    pushMessage(FIRST_MESSAGE);
 
-    // Toggles the chat window visibility
-    toggleChat() {
-      // If the conversation thread has not been initiated, initiate it
-      if (this.threadId === null) {
-        // Push a welcome message when the chat is opened
-        this.pushMessage('Hello! Welcome to Signal for Help Empowerment (SHE). How can I support you today?');
+    initiateConversationThread();
+  }
 
-        this.initiateConversationThread();
-      }
+  isOpen.value = !isOpen.value;
+  scrollToBottom();
+}
 
-      this.isOpen = !this.isOpen;
-      this.scrollToBottom();
-    },
+// Scrolls the chat to the bottom
+function scrollToBottom() {
+  nextTick(() => {
+    if (messagesContainer.value) {
+      messagesContainer.value.scrollTop = messagesContainer.value.scrollHeight;
+    }
+  });
+}
 
-    // Scrolls the chat to the bottom
-    scrollToBottom() {
-      this.$nextTick(() => {
-        const messagesContainer = this.$refs.messagesContainer;
-        messagesContainer.scrollTop = messagesContainer.scrollHeight;
-      });
-    },
+// Initiates a new conversation thread
+async function initiateConversationThread() {
+  try {
+    const response = await fetch("/api/chatbot/thread", {
+      method: 'POST',
+    });
+    const data = await response.json();
+    threadId.value = data.threadId;
+  } catch (error) {
+    // Handle errors by displaying a message to the user
+    pushMessage(ERROR_MESSAGE);
+  }
+}
 
-    // Initiates a new conversation thread
-    async initiateConversationThread() {
-      try {
-        const response = await fetch("/api/chatbot/thread", {
-          method: 'POST',
-        });
-        const data = await response.json();
-        this.threadId = data.threadId;
-      } catch (error) {
-        // Handle error by displaying a message to the user
-        this.pushMessage('An error occurred while communicating with the AI. Please try again later.');
-      }
-    },
+// Handles user input when the user presses the Enter key
+function submitMessage() {
+  if (userInput.value.trim() !== '' && taskEnded.value) {
+    taskEnded.value = false;
+    pushMessage(userInput.value, true);
 
-    // Handles user input when the user presses the Enter key
-    submitMessage() {
-      if (this.userInput.trim() !== '' && this.taskEnded) {
-        this.taskEnded = false;
-        this.pushMessage(this.userInput, true);
+    const oldUserInput = userInput.value;
+    userInput.value = ''; // Clear the input field
 
-        // Store user input locally before clearing it
-        const userInput = this.userInput;
-        this.userInput = ''; // Clear the input field
+    sendMessage(oldUserInput);
+  }
+}
 
-        this.sendMessage(userInput);
-      }
-    },
+// Sends the user message to the AI and handles the response
+async function sendMessage(userInput: string) {
+  runStatus.value = "queued";
 
-    // Sends the user message to the AI and handles the response
-    async sendMessage(userInput) {
-      this.runStatus = "queued";
+  try {
+    // Call Assistant API to send the message
+    const response = await fetch(`/api/chatbot/${threadId.value}/sendMessage`, {
+      method: 'POST',
+      body: JSON.stringify({ userInput }),
+    });
 
-      try {
-        // Call Assistant API to send the message
-        const response = await fetch(`/api/chatbot/${this.threadId}/sendMessage`, {
-          method: 'POST',
-          body: JSON.stringify({ userInput }),
-        });
+    const data = await response.json();
 
-        const data = await response.json();
+    runId.value = data.runId;
 
-        this.runId = data.runId;
+    retrieveRun();
+  } catch (error) {
+    // Handle errors by displaying a message to the user
+    pushMessage(ERROR_MESSAGE);
 
-        this.retrieveRun();
-      } catch (error) {
-        // Handle error by displaying a message to the user
-        this.pushMessage('An error occurred while communicating with the AI. Please try again later.');
+    taskEnded.value = true;
+  }
+}
 
-        this.taskEnded = true;
-      }
-    },
+// Retrieves the run status and waits for completion
+function retrieveRun() {
+  interval.value = setInterval(async () => {
+    // Display typing indicator
+    typing.value = true;
+    scrollToBottom();
 
-    // Retrieves the run status and waits for completion
-    retrieveRun() {
-      this.interval = setInterval(async () => {
-        // Display typing indicator
-        this.typing = true;
-        this.scrollToBottom();
+    const response = await fetch(`/api/chatbot/${threadId.value}/readMessage/${runId.value}`, {
+      method: 'GET',
+    });
 
-        const response = await fetch(`/api/chatbot/${this.threadId}/readMessage/${this.runId}`, {
-          method: 'GET',
-        });
+    const data = await response.json();
 
-        const data = await response.json();
+    // If the previous run status is completed or failed, stop the interval, to prevent the same message from being pushed multiple times
+    if (runStatus.value === 'completed' || runStatus.value === 'failed') {
+      clearInterval(interval.value!);
+      return;
+    }
 
-        // If the previous run status is completed or failed, stop the interval, to prevent the same message from being pushed multiple times
-        if (this.runStatus === 'completed' || this.runStatus === 'failed') {
-          clearInterval(this.interval);
-          return;
-        }
+    runStatus.value = data.runStatus;
 
-        this.runStatus = data.runStatus;
+    if (runStatus.value !== 'queued' && runStatus.value !== 'in_progress') {
+      clearInterval(interval.value!);
+      taskEnded.value = true;
+    }
 
-        if (this.runStatus !== 'queued' && this.runStatus !== 'in_progress') {
-          clearInterval(this.interval);
-          this.taskEnded = true;
-        }
+    if (runStatus.value === 'completed') {
+      typing.value = false;
+      pushMessage(data.message);
+    } else if (runStatus.value === 'failed') {
+      sendMessage(userInput.value);
+    }
+  }, RESPONSE_CHECK_INTERVAL);
+}
 
-        if (this.runStatus === 'completed') {
-          this.typing = false;
-          this.pushMessage(data.message);
-        } else if (this.runStatus === 'failed') {
-          this.sendMessage(this.userInput);
-        }
-      }, RESPONSE_CHECK_INTERVAL);
-    },
+// Pushes a message to the chat window
+function pushMessage(text: string, isUser = false) {
+  messages.value.push({ text, isUser });
 
-    pushMessage(text, isUser = false) {
-      // Add user message to the messages array
-      this.messages.push({ text, isUser });
-      // Scroll to the bottom of the chat
-      this.scrollToBottom();
-    },
-
-  },
-
-};
+  // Scroll to the bottom of the chat
+  scrollToBottom();
+}
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 200ms;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
