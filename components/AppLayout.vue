8fe8b71d9460c1fd97bc4f83f4c5a526cd879af9
carloprestifilippo <carlo.prestifilippo@mail.polimi.it>
2024-06-26 20:02:44 +0200

    footer fills exactly one page

--- components/AppLayout.vue
+++ components/AppLayout.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <AppHeader />
-    <div class="py-12">
-      <slot />
+    <div class="min-h-screen">
+      <AppHeader />
+
+      <div class="py-12">
+        <slot />
+      </div>
+
+      <AppFooter class="sticky top-[100vh]" />
     </div>
+
     <AppChatbot />
-    <AppFooter />
   </div>
 </template>
