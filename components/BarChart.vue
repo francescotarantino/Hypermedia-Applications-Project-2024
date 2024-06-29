14a0a8221ee60709aa403a5b43d77c856fb032fb
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2024-06-29 11:14:48 +0200

    BarChart responsive

--- components/BarChart.vue
+++ components/BarChart.vue
@@ -1,13 +1,28 @@
-<!-- components/ImpactChart.vue -->
+<script setup lang="ts">
+const statistics = {
+  'Number of Clients Served': 5000,
+  'Crisis Interventions': 8000,
+  'Legal Advocacy': 1500,
+  'Counseling Services': 3000,
+  'Workshops': 500,
+  'Vocational Training Placements': 400
+};
+
+function getPercentage (value: number) {
+  const max = Math.max(...Object.values(statistics));
+  return (value / max) * 100;
+}
+</script>
+
 <template>
   <div class="bg-cream shadow-md rounded-lg overflow-hidden">
     <div class="p-6">
-      <h2 class="text-lg font-semibold mb-4">Our all-time impact statistics</h2>
+      <h2 class="text-lg font-semibold mb-4 text-center sm:text-left">Our all-time impact statistics</h2>
 
       <div v-for="(data, label) in statistics" :key="label" class="mb-4">
-        <div class="flex items-center">
-          <div class="w-1/3">{{ label }}</div>
-          <div class="w-2/3 bg-peach rounded-full">
+        <div class="flex items-center flex-col md:flex-row gap-2">
+          <div class="flex-1">{{ label }}</div>
+          <div class="flex-[2_2_0%] w-full bg-peach rounded-full">
             <div :style="{ width: getPercentage(data) + '%' }" class="bg-apricot text-xs leading-none py-1 text-center text-primary rounded-full">{{ data }}</div>
           </div>
         </div>
@@ -15,21 +30,3 @@
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const statistics = {
-  'Number of Clients Served': 5000,
-  'Crisis Interventions': 10000,
-  'Legal Advocacy': 1500,
-  'Counseling Services': 3000,
-  'Workshops': 500,
-  'Vocational Training Placements': 300
-};
-
-const getPercentage = (value: number) => {
-  const max = Math.max(...Object.values(statistics));
-  return (value / max) * 100;
-};
-</script>
