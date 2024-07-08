545ef948ef7b067a180d4cac0905ae4b1766b3f2
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2024-07-08 18:15:40 +0200

    Restored Map component

--- components/Map.vue
+++ components/Map.vue
@@ -9,11 +9,10 @@ const zoom = ref(16);
 <template>
   <LMap class="rounded-xl" :zoom="zoom" :center="coordinates" :use-global-leaflet="false" :options="{ scrollWheelZoom: false }">
     <LTileLayer
-        url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
-        attribution="&copy; <a href=&quot;https://www.stadiamaps.com/&quot;>Stadia Maps</a> &copy; <a href=&quot;https://openmaptiles.org/&quot;>OpenMapTiles</a> &copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
         layer-type="base"
         name="OpenStreetMap"
-        :max-zoom="20"
     />
     <LMarker :lat-lng="coordinates" />
   </LMap>
