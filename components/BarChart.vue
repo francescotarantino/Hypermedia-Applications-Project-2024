2ef21ee968d647dbeacdde8c066f1617b7a43380
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2024-07-08 12:57:22 +0200

    Animated BarChart

--- components/BarChart.vue
+++ components/BarChart.vue
@@ -7,10 +7,32 @@ function getPercentage (value: number) {
   const max = Math.max(...Object.values(props.statistics));
   return (value / max) * 100;
 }
+
+// Using Intersection Observer API to show the bars when the chart is in the viewport
+const showBars = ref(false);
+onMounted(() => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        showBars.value = true;
+      }
+    });
+  }, {
+    root: null, // observing with respect to the viewport
+    threshold: 0.5 // callback will be executed when 50% of the element is visible
+  });
+
+  const element = document.getElementById('bar-chart');
+  if (element) observer.observe(element);
+
+  onUnmounted(() => {
+    observer.disconnect();
+  });
+});
 </script>
 
 <template>
-  <div class="bg-cream shadow-md rounded-lg overflow-hidden">
+  <div class="bg-cream shadow-md rounded-lg overflow-hidden" id="bar-chart">
     <div class="p-6">
       <h2 class="text-lg font-semibold mb-4 text-center sm:text-left">Our all-time impact statistics</h2>
 
@@ -19,7 +41,9 @@ function getPercentage (value: number) {
           <div class="flex-1">{{ label }}</div>
 
           <div class="flex-[2_2_0%] w-full bg-peach rounded-full">
-            <div :style="{ width: getPercentage(data) + '%' }" class="min-w-8 bg-apricot text-xs leading-none py-1 text-center rounded-full">
+            <div class="min-w-8 bg-apricot text-xs leading-none py-1 text-center rounded-full transition-all ease-in-out duration-[2s]"
+                 :style="{ width: showBars ? `${getPercentage(data)}%` : '0px' }"
+            >
               {{ data }}
             </div>
           </div>
