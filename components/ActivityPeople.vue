86d21f5f0884b95d5fd9dbbf7b30798d791c9943
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2024-07-09 12:00:01 +0200

    Scrolling people cards on smaller devices

--- components/ActivityPeople.vue
+++ components/ActivityPeople.vue
@@ -8,10 +8,22 @@ defineProps<{
   <div class="flex flex-col gap-8">
     <h2 class="text-2xl text-center text-orange font-bold">People involved</h2>
 
-    <div class="flex flex-wrap self-center justify-center gap-4 md:w-3/4">
-      <VerticalPersonCard :person="activity.main_responsible!" :isMainResponsible="true" />
+    <div class="flex sm:justify-center overflow-x-scroll overflow-y-hidden scrollbar-hidden pt-4">
+      <div class="flex flex-nowrap sm:flex-wrap px-10 pb-14 sm:ms-0 sm:px-0 lg:px-0 self-center justify-center gap-4 md:w-3/4">
+        <VerticalPersonCard :person="activity.main_responsible!" :isMainResponsible="true" />
 
-      <VerticalPersonCard v-for="person in activity.people_involved!" :person="person" />
+        <VerticalPersonCard v-for="person in activity.people_involved!" :person="person" />
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.scrollbar-hidden::-webkit-scrollbar {
+  display: none;
+}
+
+.scrollbar-hidden {
+  scrollbar-width: none;
+}
+</style>
