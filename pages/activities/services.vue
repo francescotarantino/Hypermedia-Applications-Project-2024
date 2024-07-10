e01e817a4cc62ec260d84e1e7d4b9763d5bec4b9
carloprestifilippo <carlo.prestifilippo@mail.polimi.it>
2024-07-10 21:42:08 +0200

    services bold

--- pages/activities/services.vue
+++ pages/activities/services.vue
@@ -13,12 +13,12 @@ useSeoMeta({
       <h1 class="text-3xl font-semibold text-center text-orange">Services</h1>
       <p class="text-justify text-lg text-gray-600 mt-8 mb-12">
         At SHE Centre we provide a range of essential resources and support tailored to empower women facing domestic violence. 
-        Our services have evolved over the years to encompass a comprehensive approach to healing and empowerment. 
-        From crisis intervention and safe shelter to legal advocacy, skills training, and community outreach, each service 
-        is designed to meet the unique needs of survivors on their journey towards safety and independence. <br>
-        Explore our diverse range of services and discover how the SHE Centre can support you or someone you know in navigating 
+        Our services have <b>evolved over the years</b> to encompass a comprehensive approach to healing and empowerment.
+        From <b>crisis intervention</b> and <b>safe shelter</b> to <b>legal advocacy</b>, <b>skills training</b>, and <b>community outreach</b>, each service
+        is designed to meet the unique needs of survivors on their journey towards safety and independence.
+        Explore our diverse range of services and discover how the SHE Centre can <b>support you</b> or someone you know in navigating
         the challenges of domestic violence. Whether you are seeking shelter, legal guidance, skills development, or community 
-        engagement, we are here to provide compassionate care and practical assistance every step of the way.
+        engagement, <b>we are here</b> to provide compassionate care and practical assistance every step of the way.
       </p>
       <div class="grid justify-items-center">
         <div class="max-w-screen-lg grid grid-cols-1 md:grid-cols-2 gap-4">
